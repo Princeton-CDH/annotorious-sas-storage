ea078ce5329025d1735c8dad067092b6c8334036
rlskoeser <rebecca.s.koeser@princeton.edu>
2022-04-29 16:40:55 -0400

    Adjust SAS code to work with Annotorious

--- src/SimpleAnnotationServerV2Adapter.js
+++ src/SimpleAnnotationServerV2Adapter.js
@@ -1,4 +1,10 @@
-/** */
+/* Convert between v2 (open annotation) and v3 (w3c) annotations
+   in order to bridge between annotorious (w3c) and simple annotation server (v2).
+
+   Adapted from mirador-annotations code
+   https://github.com/ProjectMirador/mirador-annotations/blob/master/src/SimpleAnnotationServerV2Adapter.js
+
+*/
 export default class SimpleAnnotationServerV2Adapter {
   /** */
   constructor(canvasId, endpointUrl) {
@@ -14,26 +20,32 @@ export default class SimpleAnnotationServerV2Adapter {
   /** */
   async create(annotation) {
     return fetch(`${this.endpointUrl}/create`, {
-      body: JSON.stringify(SimpleAnnotationServerV2Adapter.createV2Anno(annotation)),
+            body: JSON.stringify(
+                SimpleAnnotationServerV2Adapter.createV2Anno(annotation)
+            ),
       headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
+                Accept: "application/json",
+                "Content-Type": "application/json",
       },
-      method: 'POST',
+            method: "POST",
+        })
+            .then((response) => {
+                return this.all();
     })
-      .then((response) => this.all())
       .catch(() => this.all());
   }
 
   /** */
   async update(annotation) {
     return fetch(`${this.endpointUrl}/update`, {
-      body: JSON.stringify(SimpleAnnotationServerV2Adapter.createV2Anno(annotation)),
+            body: JSON.stringify(
+                SimpleAnnotationServerV2Adapter.createV2Anno(annotation)
+            ),
       headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
+                Accept: "application/json",
+                "Content-Type": "application/json",
       },
-      method: 'POST',
+            method: "POST",
     })
       .then((response) => this.all())
       .catch(() => this.all());
@@ -41,13 +53,16 @@ export default class SimpleAnnotationServerV2Adapter {
 
   /** */
   async delete(annoId) {
-    return fetch(`${this.endpointUrl}/destroy?uri=${encodeURIComponent(annoId)}`, {
+        return fetch(
+            `${this.endpointUrl}/destroy?uri=${encodeURIComponent(annoId)}`,
+            {
       headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
       },
-      method: 'DELETE',
-    })
+                method: "DELETE",
+            }
+        )
       .then((response) => this.all())
       .catch(() => this.all());
   }
@@ -72,17 +87,17 @@ export default class SimpleAnnotationServerV2Adapter {
   /** Creates a V2 annotation from a V3 annotation */
   static createV2Anno(v3anno) {
     const v2anno = {
-      '@context': 'http://iiif.io/api/presentation/2/context.json',
-      '@type': 'oa:Annotation',
-      motivation: 'oa:commenting',
+            "@context": "http://iiif.io/api/presentation/2/context.json",
+            "@type": "oa:Annotation",
+            motivation: "oa:commenting", // todo use v3anno.motivation
       on: {
-        '@type': 'oa:SpecificResource',
+                "@type": "oa:SpecificResource",
         full: v3anno.target.source.id,
       },
     };
     // copy id if it is SAS-generated
-    if (v3anno.id && v3anno.id.startsWith('http')) {
-      v2anno['@id'] = v3anno.id;
+        if (v3anno.id && v3anno.id.startsWith("http")) {
+            v2anno["@id"] = v3anno.id;
     }
     if (Array.isArray(v3anno.body)) {
       v2anno.resource = v3anno.body.map((b) => this.createV2AnnoBody(b));
@@ -91,20 +106,24 @@ export default class SimpleAnnotationServerV2Adapter {
     }
     if (v3anno.target.selector) {
       if (Array.isArray(v3anno.target.selector)) {
-        const selectors = v3anno.target.selector.map((s) => this.createV2AnnoSelector(s));
+                const selectors = v3anno.target.selector.map((s) =>
+                    this.createV2AnnoSelector(s)
+                );
         // create choice, assuming two elements and 0 is default
         v2anno.on.selector = {
-          '@type': 'oa:Choice',
+                    "@type": "oa:Choice",
           default: selectors[0],
           item: selectors[1],
         };
       } else {
-        v2anno.on.selector = this.createV2AnnoSelector(v3anno.target.selector);
+                v2anno.on.selector = this.createV2AnnoSelector(
+                    v3anno.target.selector
+                );
       }
       if (v3anno.target.source.partOf) {
         v2anno.on.within = {
-          '@id': v3anno.target.source.partOf.id,
-          '@type': 'sc:Manifest',
+                    "@id": v3anno.target.source.partOf.id,
+                    "@type": "sc:Manifest",
         };
       }
     }
@@ -116,10 +135,10 @@ export default class SimpleAnnotationServerV2Adapter {
     const v2body = {
       chars: v3body.value,
     };
-    if (v3body.purpose === 'tagging') {
-      v2body['@type'] = 'oa:Tag';
+        if (v3body.purpose === "tagging") {
+            v2body["@type"] = "oa:Tag";
     } else {
-      v2body['@type'] = 'dctypes:Text';
+            v2body["@type"] = "dctypes:Text";
     }
     if (v3body.format) {
       v2body.format = v3body.format;
@@ -133,15 +152,16 @@ export default class SimpleAnnotationServerV2Adapter {
   /** */
   static createV2AnnoSelector(v3selector) {
     switch (v3selector.type) {
-      case 'SvgSelector':
+            case "SvgSelector":
         return {
-          '@type': 'oa:SvgSelector',
+                    "@type": "oa:SvgSelector",
           value: v3selector.value,
         };
-      case 'FragmentSelector':
+            case "FragmentSelector":
         return {
-          '@type': 'oa:FragmentSelector',
-          value: v3selector.value,
+                    "@type": "oa:FragmentSelector",
+                    // SAS uses location in xywh=x,y,w,h format; annotorious uses pixel:x,y,w,h
+                    value: v3selector.value.replace("xywh=pixel:", "xywh="),
         };
       default:
         return null;
@@ -151,11 +171,13 @@ export default class SimpleAnnotationServerV2Adapter {
   /** Creates an AnnotationPage from a list of V2 annotations */
   createAnnotationPage(v2annos) {
     if (Array.isArray(v2annos)) {
-      const v3annos = v2annos.map((a) => SimpleAnnotationServerV2Adapter.createV3Anno(a));
+            const v3annos = v2annos.map((a) =>
+                SimpleAnnotationServerV2Adapter.createV3Anno(a)
+            );
       return {
         id: this.annotationPageId,
         items: v3annos,
-        type: 'AnnotationPage',
+                type: "AnnotationPage",
       };
     }
     return v2annos;
@@ -164,9 +186,10 @@ export default class SimpleAnnotationServerV2Adapter {
   /** Creates a V3 annotation from a V2 annotation */
   static createV3Anno(v2anno) {
     const v3anno = {
-      id: v2anno['@id'],
-      motivation: 'commenting',
-      type: 'Annotation',
+            "@context": "http://www.w3.org/ns/anno.jsonld",
+            id: v2anno["@id"],
+            motivation: "commenting",
+            type: "Annotation",
     };
     if (Array.isArray(v2anno.resource)) {
       v3anno.body = v2anno.resource.map((b) => this.createV3AnnoBody(b));
@@ -185,10 +208,10 @@ export default class SimpleAnnotationServerV2Adapter {
       v3anno.target.source = {
         id: v2target.full,
         partOf: {
-          id: v2target.within['@id'],
-          type: 'Manifest',
+                    id: v2target.within["@id"],
+                    type: "Manifest",
         },
-        type: 'Canvas',
+                type: "Canvas",
       };
     }
     return v3anno;
@@ -197,7 +220,7 @@ export default class SimpleAnnotationServerV2Adapter {
   /** */
   static createV3AnnoBody(v2body) {
     const v3body = {
-      type: 'TextualBody',
+            type: "TextualBody",
       value: v2body.chars,
     };
     if (v2body.format) {
@@ -206,26 +229,28 @@ export default class SimpleAnnotationServerV2Adapter {
     if (v2body.language) {
       v3body.language = v2body.language;
     }
-    if (v2body['@type'] === 'oa:Tag') {
-      v3body.purpose = 'tagging';
+        if (v2body["@type"] === "oa:Tag") {
+            v3body.purpose = "tagging";
     }
     return v3body;
   }
 
   /** */
   static createV3AnnoSelector(v2selector) {
-    switch (v2selector['@type']) {
-      case 'oa:SvgSelector':
+        switch (v2selector["@type"]) {
+            case "oa:SvgSelector":
         return {
-          type: 'SvgSelector',
+                    type: "SvgSelector",
           value: v2selector.value,
         };
-      case 'oa:FragmentSelector':
+            case "oa:FragmentSelector":
         return {
-          type: 'FragmentSelector',
-          value: v2selector.value,
+                    type: "FragmentSelector",
+                    conformsTo: "http://www.w3.org/TR/media-frags/",
+                    // SAS returns location in xywh=x,y,w,h format; annotorious uses pixel:x,y,w,h
+                    value: v2selector.value.replace("xywh=", "xywh=pixel:"),
         };
-      case 'oa:Choice':
+            case "oa:Choice":
         /* create alternate selectors */
         return [
           this.createV3AnnoSelector(v2selector.default),

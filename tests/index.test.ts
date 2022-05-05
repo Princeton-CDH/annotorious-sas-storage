44e93fa5a63fac141c3e9e44f96de08f94be865b
Ben Silverman <ben@performantsoftware.com>
2022-05-05 12:18:54 -0400

    Improve test organization

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -28,34 +28,74 @@ const adapterMock =
     SimpleAnnotationServerV2Adapter as unknown as jest.Mock<SimpleAnnotationServerV2Adapter>;
 
 // Mock the Annotorious client
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const eventArray: { name: string; fn: () => any }[] = [];
 const clientMock = {
     setAnnotations: jest.fn(),
-    on: jest.fn(),
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    on: jest.fn().mockImplementation((evtName: string, handler: () => any) => {
+        // add name/handler pair to events array
+        eventArray.push({ name: evtName, fn: handler });
+    }),
+    emit: jest.fn().mockImplementation((evtName: string) => {
+        // match event name to a handler in events array
+        eventArray.find((evt) => evt.name === evtName)?.fn();
+    }),
 };
 
-test("should instantiate the plugin and dispatch the anno load event", async () => {
-    const settings = {
-        annotationEndpoint: "fakeEndpoint",
-        manifest: "fakeManifest",
-        target: "fakeCanvas",
-    };
-    const dispatchEventSpy = jest.spyOn(document, "dispatchEvent");
-    AnnotationServerStorage(clientMock, settings);
+// mock settings object
+const settings = {
+    annotationEndpoint: "fakeEndpoint",
+    manifest: "fakeManifest",
+    target: "fakeCanvas",
+};
 
-    // Should instantiate the adapter with the above settings
-    expect(adapterMock).toHaveBeenCalled();
-    expect(adapterMock).toHaveBeenCalledWith("fakeCanvas", "fakeEndpoint");
+describe("Plugin instantiation", () => {
+    beforeEach(() => {
+        // Reset mocks before each test
+        adapterMock.mockClear();
+        clientMock.on.mockClear();
+        clientMock.setAnnotations.mockClear();
+    });
 
-    // ensure then() is called
-    await Promise.resolve();
+    it("Should instantiate the adapter with the above settings", () => {
+        // initialize the storage
+        AnnotationServerStorage(clientMock, settings);
+        expect(adapterMock).toHaveBeenCalled();
+        expect(adapterMock).toHaveBeenCalledWith("fakeCanvas", "fakeEndpoint");
+    });
 
-    // should call setAnnotations with the empty array
-    expect(clientMock.setAnnotations).toHaveBeenCalled();
-    expect(clientMock.setAnnotations.mock.calls[0][0]).toStrictEqual([]);
+    it("Should dispatch the anno load event", async () => {
+        // spy on dispatch event to ensure it is called later
+        const dispatchEventSpy = jest.spyOn(document, "dispatchEvent");
 
-    // should dispatch the event "annotations-loaded"
-    expect(adapterMock).toHaveBeenCalled();
-    expect(dispatchEventSpy).toHaveBeenCalledWith(
-        new Event("annotations-loaded"),
-    );
+        // initialize the storage
+        AnnotationServerStorage(clientMock, settings);
+        // ensure then() is called
+        await Promise.resolve();
+
+        // Should dispatch the event "annotations-loaded"
+        expect(dispatchEventSpy).toHaveBeenCalledWith(
+            new Event("annotations-loaded"),
+        );
+    });
+
+    it("Should call setAnnotations with an empty array from settings", async () => {
+        // initialize the storage
+        AnnotationServerStorage(clientMock, settings);
+        // ensure then() is called
+        await Promise.resolve();
+
+        expect(clientMock.setAnnotations).toHaveBeenCalled();
+        expect(clientMock.setAnnotations.mock.calls[0][0]).toStrictEqual([]);
+    });
+
+    it("Should initialize the event listeners", async () => {
+        // initialize the storage
+        AnnotationServerStorage(clientMock, settings);
+        // ensure then() is called
+        await Promise.resolve();
+
+        expect(clientMock.on).toHaveBeenCalledTimes(3);
+    });
 });

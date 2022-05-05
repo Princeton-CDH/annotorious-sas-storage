f7b3e8e03d113f889f78a64e443b3a71a8cd0b6f
Ben Silverman <ben@performantsoftware.com>
2022-05-05 13:11:49 -0400

    Add test for createAnnotation event handler

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,25 +1,45 @@
 import AnnotationServerStorage from "../src/index";
+import { Annotation } from "../src/types/V3/Annotation";
 import SimpleAnnotationServerV2Adapter from "../src/utils/SimpleAnnotationServerV2Adapter";
+import adjustTargetSource from "../src/utils/adjustTargetSource";
 
-// Mock the adapter
+// Mock the adjustTargetSource util
+jest.mock("../src/utils/adjustTargetSource", () => {
+    return {
+        __esModule: true,
+        default: jest.fn(),
+    };
+});
+const adjustTargetSourceMock = adjustTargetSource as jest.Mock;
+
+// Mock the V2-V3 adapter
+const mockCreate = jest
+    .fn()
+    .mockImplementation(
+        (annotation: Annotation) =>
+            new Promise((resolve) => resolve(annotation)),
+    );
+const mockAll = jest.fn().mockImplementation(
+    () =>
+        new Promise((resolve) =>
+            resolve({
+                id: "test",
+                items: [],
+                type: "AnnotationPage",
+            }),
+        ),
+);
+const mockUpdate = jest.fn();
+const mockDelete = jest.fn();
 jest.mock("../src/utils/SimpleAnnotationServerV2Adapter", () => {
     return {
         __esModule: true,
         default: jest.fn().mockImplementation(() => ({
             // Mock the Promise and data returned by all()
-            all: jest.fn().mockImplementation(
-                () =>
-                    new Promise((resolve) =>
-                        resolve({
-                            id: "test",
-                            items: [],
-                            type: "AnnotationPage",
-                        }),
-                    ),
-            ),
-            create: jest.fn(),
-            update: jest.fn(),
-            delete: jest.fn(),
+            all: mockAll,
+            create: mockCreate,
+            update: mockUpdate,
+            delete: mockDelete,
         })),
     };
 });
@@ -29,17 +49,19 @@ const adapterMock =
 
 // Mock the Annotorious client
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const eventArray: { name: string; fn: () => any }[] = [];
+const eventArray: { name: string; fn: (data?: any) => any }[] = [];
 const clientMock = {
+    addAnnotation: jest.fn(),
     setAnnotations: jest.fn(),
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     on: jest.fn().mockImplementation((evtName: string, handler: () => any) => {
         // add name/handler pair to events array
         eventArray.push({ name: evtName, fn: handler });
     }),
-    emit: jest.fn().mockImplementation((evtName: string) => {
-        // match event name to a handler in events array
-        eventArray.find((evt) => evt.name === evtName)?.fn();
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    emit: jest.fn().mockImplementation((evtName: string, data?: any) => {
+        // match event name to a handler in events array, pass data
+        eventArray.find((evt) => evt.name === evtName)?.fn(data);
     }),
 };
 
@@ -97,5 +119,35 @@ describe("Plugin instantiation", () => {
         await Promise.resolve();
 
         expect(clientMock.on).toHaveBeenCalledTimes(3);
+    });
+});
+
+describe("Event handlers", () => {
+    beforeEach(() => {
+        // Reset mocks before each test
+        adapterMock.mockClear();
+        clientMock.on.mockClear();
+        clientMock.emit.mockClear();
+        // initialize the storage
+        AnnotationServerStorage(clientMock, settings);
+    });
+    it("should respond to emitted createAnnotation event with handler", async () => {
+        const annotation = {
+            "@context": "fakeContext",
+            body: {},
+            motivation: "commenting",
+            target: { source: "fakesource" },
+            type: "Annotation",
+        };
+        clientMock.emit("createAnnotation", annotation);
+        // should call AdjustTargetSource
+        expect(adjustTargetSourceMock).toHaveBeenCalledWith(
+            "fakesource",
+            settings,
+        );
+        // should call adapter.create
+        expect(mockCreate).toHaveBeenCalledWith(annotation);
+        // should call addAnnotation on client
+        expect(clientMock.addAnnotation).toHaveBeenCalledWith(annotation);
     });
 });
